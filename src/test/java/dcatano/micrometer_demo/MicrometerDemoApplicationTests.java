defca5e124119882d438549a1455a5f3f811b3cc
Daniel Cataño Restrepo <dasniel199410@gmail.com>
2024-03-22 14:06:02 -0500

    Commit inicial

---
+++ src/test/java/dcatano/micrometer_demo/MicrometerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package dcatano.micrometer_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicrometerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
